c1a7a27338e3f53d174bf7b06fb80b431f1aba3f
Clara Aw <clara.aw.jq@gmail.com>
2023-03-14 03:06:19 +0800

    test component to try and link back and frontend

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <p>
+            {{ msg }}
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'TestComponent',
+    data() {
+        return {
+            msg : "trying not to kill myself"
+        };
+    }
+}
+</script>
