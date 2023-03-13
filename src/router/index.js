539b2687dad532acb61119e217025183150aa708
Clara Aw <clara.aw.jq@gmail.com>
2023-03-14 03:06:43 +0800

    updating new path for testcomponent

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import TestComponent from '../components/TestComponent.vue'
 
 const routes = [
@@ -11,7 +11,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 });
 
