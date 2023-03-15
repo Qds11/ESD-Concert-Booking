2ed7ab018744befce46396d521c1c1f07d7b11f1
Clara Aw <clara.aw.jq@gmail.com>
2023-03-15 21:17:18 +0800

    adding submit button

---
+++ src/components/shared/SubmitButton.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-btn variant="tonal">{{ action }}</v-btn>
+</template>
+
+<script>
+export default{
+    name:"SubmitButton",
+    props: ['action']
+}
+</script>
+
+<style>
+.btn{
+    color: black;
+}
+</style>
