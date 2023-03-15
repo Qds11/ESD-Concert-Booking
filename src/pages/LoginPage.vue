c61af2d182d17b20eb82a50631e5f01b0774bfd6
Clara Aw <clara.aw.jq@gmail.com>
2023-03-15 20:38:59 +0800

    updated login background colour

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,39 +1,28 @@
 <template>
-  <v-container id="bg" fluid fill-height>
-    <v-row align="center" style="height: 100vh">
-      <v-col no-gutters cols="7">
-        <v-img
-          fluid
-          :src="require('../assets/concerts/concert4.jpeg')"
-          class="img"
-        >
-        </v-img>
-      </v-col>
+    <v-container id="bg" fluid fill-height>
+      <v-row align="center" style="height: 100vh">
+        <v-col no-gutters cols="7">
+          <v-img fluid :src="require('../assets/concerts/concert4.jpeg')" class="img">
+          </v-img>
+        </v-col>
 
-      <v-col cols="5">
-        <v-sheet color="black" fluid fill-height class="mx-auto"> 
-          <v-form @submit.prevent>
-            <v-text-field
-              v-model="firstName"
-              label="First name"
-            ></v-text-field>
+        <v-col cols="5">
+          <v-sheet color="black" fluid fill-height class="mx-auto">
+            <v-form @submit.prevent>
+              <v-text-field v-model="firstName" label="First name"></v-text-field>
 
-            <v-text-field
-              v-model="password"
-              label="password"
-            ></v-text-field>
+              <v-text-field v-model="password" label="password"></v-text-field>
 
-            <v-btn type="submit" block class="mt-2">Submit</v-btn>
-          </v-form>
-        </v-sheet>
+              <v-btn type="submit" block class="mt-2">Submit</v-btn>
+            </v-form>
+          </v-sheet>
 
-      </v-col>
-    </v-row>
-  </v-container>
+        </v-col>
+      </v-row>
+    </v-container>
 </template>
 
 <script>
-// import NavBar from "../components/shared/NavBar.vue";
 
 export default {
   name: "LoginPage",
@@ -46,11 +35,16 @@ export default {
 
 <style>
 #bg {
-  background-color: black;
+  background-image: linear-gradient(#0d0d33, #01010c, #040311);
+  height: 100vh;
+  background-repeat: no-repeat;
+
 }
+
 .img {
   height: 100vh;
 }
+
 html {
   background-color: black;
 }
