53f08b95dea44ef1368eb3e7b6fc1604fe6d2c9a
Clara Aw <clara.aw.jq@gmail.com>
2023-03-15 23:05:06 +0800

    added google login

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -13,13 +13,21 @@
       <v-col cols="6" align-self="center" class="pa-5">
         <v-sheet color="black" fluid>
           <v-container>
-            <p class="text-h4 mb-10" style="columns: white;">Welcome to TicketPRO!</p>
+            <p class="text-h4 mb-10" style="columns: white">
+              Welcome to TicketPRO!
+            </p>
           </v-container>
 
           <v-form @submit.prevent>
-            <v-text-field v-model="username" label="Enter username"></v-text-field>
-      
-            <v-text-field v-model="password" label="Enter password"></v-text-field>
+            <v-text-field
+              v-model="username"
+              label="Enter username"
+            ></v-text-field>
+
+            <v-text-field
+              v-model="password"
+              label="Enter password"
+            ></v-text-field>
 
             <v-row class="mt-9">
               <v-col>
@@ -27,10 +35,15 @@
               </v-col>
 
               <v-col>
-                <SubmitButton action="Register" />
+                <GoogleLogin :callback="callback"/>
               </v-col>
 
+              <v-col>
+                <SubmitButton action="Register" />
+              </v-col>
+              
             </v-row>
+
           </v-form>
         </v-sheet>
       </v-col>
@@ -38,8 +51,17 @@
   </v-container>
 </template>
 
+<script setup>
+const callback = (response) => {
+  // This callback will be triggered when the user selects or login to
+  // his Google account from the popup
+  console.log("Handle the response", response)
+}
+</script>
+
+
 <script>
-import SubmitButton from "../components/shared/SubmitButton.vue";
+import SubmitButton from '@/components/shared/SubmitButton.vue';
 
 export default {
   name: "LoginPage",
@@ -49,6 +71,7 @@ export default {
   data() {
     return {};
   },
+
 };
 </script>
 
@@ -62,4 +85,5 @@ export default {
 html {
   background-color: black;
 }
+
 </style>
