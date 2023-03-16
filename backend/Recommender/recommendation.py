21eb405b4ddca3b0149fc03e985f3cc7b885081c
xhihyu <xhihyuxxx@gmail.com>
2023-03-16 15:27:31 +0800

    created ticketing and user db, added some code in recommender

---
+++ backend/Recommender/recommendation.py
@@ -0,0 +1,79 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:8889/users'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+
+
+    user_id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    contact = db.Column(db.Integer, nullable=False)
+    joined_date_time=db.Column(db.DateTime, nullable=False)
+    birthdate=db.Column(db.DateTime,nullable=False)
+    genre_preferred=db.Column(db.String(64),nullable=False)
+
+
+    def __init__(self, user_id, username, email, contact, joined_date_time, birthdate, genre_preferred):
+        self.user_id = user_id
+        self.username = username
+        self.email = email
+        self.contact = contact
+        self.joined_date_time=joined_date_time
+        self.birthdate=birthdate
+        self.genre_preferred=genre_preferred
+
+
+    def json(self):
+        return {"user_id": self.user_id, "username": self.username, "email": self.email, "contact": self.contact, "joined_date_time":self.joined_date_time, "birthdate":self.birthdate, "genre_preferred":self.genre_preferred}
+
+@app.route("/book")
+def get_all():
+    users = User.query.all()
+    if len(users):
+        return jsonify(
+            {
+                "code": 200,
+                "data": {
+                    "users": [user.json() for user in users]
+                }
+            }
+        )
+    return jsonify(
+        {
+            "code": 404,
+            "message": "There are no books."
+        }
+    ), 404
+
+
+
+@app.route("/book/<string:isbn13>")
+def find_by_user_id(user_id):
+    user = User.query.filter_by(user_id=user_id).first()
+    if user:
+        return jsonify(
+            {
+                "code": 200,
+                "data": user.json()
+            }
+        )
+    return jsonify(
+        {
+            "code": 404,
+            "message": "Book not found."
+        }
+    ), 404
+
+
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
---
+++ backend/Recommender/ticketing.sql
@@ -0,0 +1,29 @@
+ drop database if exists ticketing;
+
+create database ticketing;
+use ticketing;
+
+CREATE TABLE IF NOT EXISTS ticket (
+ticket_id int NOT NULL AUTO_INCREMENT,
+num_tickets int NOT NULL,
+user_id int NOT NULL,
+username TEXT,
+date_time DATETIME,
+artist TEXT,
+concert_name TEXT,
+price DECIMAL(10,2),
+hall_id int,
+ticket_sale_date_time DATETIME,
+description TEXT,
+image_path TEXT,
+PRIMARY KEY(ticket_id)
+);
+
+INSERT INTO ticket (num_tickets, user_id, username, date_time, artist, concert_name, price, hall_id, ticket_sale_date_time,description,image_path) VALUES
+(3,1,'jigglypuff','2023-06-23T19:30:00','txt','TXT Sweet Mirage Tour 2023' ,168.00,1,'2023-03-20T14:30:00','Hottest 4th Gen Kpop Group Finally in Singapore!','concert1.jpg'),
+(5,1,'jigglypuff','2023-10-15T19:00:00','Mr Cho','Cho Sweet Strings' ,50.00,1,'2023-05-20T14:30:00','Listen to the beautiful violin melodies by the classic Mr Cho','concert2.jpg'),
+(2,5,'squirtle','2023-09-29T19:00:00','Lil Pip','Pip Install FTW' ,90.00,2,'2023-07-20T14:30:00','Come listen to aggressive rap about python at its finest.','concert3.jpg'),
+(1,3,'snorlax','2023-09-29T19:00:00','Lil Pip','Pip Install FTW' ,90.00,2,'2023-07-20T14:30:00','Come listen to aggressive rap about python at its finest.','concert3.jpg'),
+(4,2,'pikachu','2023-10-15T19:00:00','Mr Cho','Cho Sweet Strings' ,50.00,1,'2023-05-20T14:30:00','Listen to the beautiful violin melodies by the classic Mr Cho','concert2.jpg'),
+(2,4,'mew','2023-09-29T19:00:00','Lil Pip','Pip Install FTW' ,90.00,2,'2023-07-20T14:30:00','Come listen to aggressive rap about python at its finest.','concert3.jpg')
+
