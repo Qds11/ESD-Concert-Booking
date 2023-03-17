65e77021ae539b5b90b3299a3ec831103bd2ec10
Clara Aw <clara.aw.jq@gmail.com>
2023-03-18 02:00:45 +0800

    updated jigglypuff email

--- backend/User/user.sql
+++ backend/User/user.sql
@@ -14,7 +14,7 @@ PRIMARY KEY(user_id)
 );
 
 INSERT INTO user (username, email, contact, joined_date_time, birthdate, genre_preferred) VALUES
-('jigglypuff','teamjigglypuff355@gmail.com', 97881010,'2023-03-16T14:04:23' ,'2002-05-20','K-POP'),
+('jigglypuff','teamjigglypuff335@gmail.com', 97881010,'2023-03-16T14:04:23' ,'2002-05-20','K-POP'),
 ('pikachu','pikachu123@gmail.com', 88181688,'2023-03-16T14:04:30' ,'2001-01-01','Classical'),
 ('snorlax','snorlax@hotmail.com', 99192939,'2023-03-16T13:18:28' ,'2002-01-30','Rap'),
 ('mew','mew@yahoo.com', 98981919,'2023-03-16T13:55:25' ,'2003-03-03','K-POP'),
