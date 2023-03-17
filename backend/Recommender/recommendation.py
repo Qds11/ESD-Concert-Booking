7ff95a07241c9aba7b6428d377c628c4813aab22
xhihyu <xhihyuxxx@gmail.com>
2023-03-17 23:14:23 +0800

    added cors to recommendation

--- backend/Recommender/recommendation.py
+++ backend/Recommender/recommendation.py
@@ -1,12 +1,15 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:8889/users'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:3306/users'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+CORS(app)
 
 class User(db.Model):
     __tablename__ = 'user'
