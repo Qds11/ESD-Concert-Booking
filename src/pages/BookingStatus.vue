db4584b1ab29e28e92f232a934f0c932081e87f5
xhihyu <xhihyuxxx@gmail.com>
2023-03-19 11:15:15 +0800

    idk

--- src/pages/BookingStatus.vue
+++ src/pages/BookingStatus.vue
@@ -24,7 +24,7 @@
     name: "SuccessPage",
     data() {
     return {
-      isSuccessful: false, // Set this to true or false based on whether the booking was successful
+      isSuccessful: true, // Set this to true or false based on whether the booking was successful
     };
   },
   };
