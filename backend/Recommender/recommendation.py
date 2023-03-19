2345c12d7b0c20019c341ca4be5cb1ca187bf130
Qds11 <limqiqing96@gmail.com>
2023-03-19 17:07:30 +0800

    concert communication with reco

--- backend/Recommender/recommendation.py
+++ backend/Recommender/recommendation.py
@@ -4,9 +4,9 @@ from flask_cors import CORS
 import requests
 from invokes import invoke_http
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://sql12606226:61vMwF9lhJ@sql12.freesqldatabase.com:3306/sql12606226'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://sql12606226:61vMwF9lhJ@sql12.freesqldatabase.com:3306/sql12606226'
 # for local db
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/ticket_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/ticket_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -18,7 +18,8 @@ CORS(app)
 
 @app.route("/user/<string:user_id>")
 def find_genre_by_calling_user(user_id):
-    results = invoke_http("http://localhost:5000/user/"+user_id, method='GET')
+    print(user_id)
+    results = invoke_http("http://127.0.0.1:5000/user/"+user_id, method='GET')
     if results['code']==200:
         return jsonify(
         {
