cad4eb369b692d314c04b46c8689867c7e7230b9
xhihyu <xhihyuxxx@gmail.com>
2023-03-19 16:29:38 +0800

    completed recc ms part 1, qiqing will call my ms and i will call clara ms and return result to qq

--- backend/Recommender/recommendation.py
+++ backend/Recommender/recommendation.py
@@ -3,23 +3,31 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import requests
 from invokes import invoke_http
-results = invoke_http("http://localhost:5000/user/1", method='GET')
-
-
-# app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:8889/ticket_db'
-# # for local db
-# # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/ticket_db'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# db = SQLAlchemy(app)
-
 app = Flask(__name__)
 CORS(app)
 
-print( type(results) )
-print()
-print( results )
+
+
+
+@app.route("/user/<string:user_id>")
+def find_genre_by_calling_user(user_id):
+    results = invoke_http("http://localhost:5000/user/"+user_id, method='GET')
+    if results['code']==200:
+        return jsonify(
+        {
+            "code": 200,
+            "message": results['message']
+        }
+    ), 200
+    return jsonify(
+        {
+            "code": 404,
+            "message": "User not found."
+        }
+    ), 404
+
+
+
 
 
 
