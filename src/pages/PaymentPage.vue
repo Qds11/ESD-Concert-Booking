9f7f3f9a11bccae72cd33e5694a215c85b0813ea
Pine <leeleetan0808@gmail.com>
2023-04-03 01:04:57 +0800

    test

--- src/pages/PaymentPage.vue
+++ src/pages/PaymentPage.vue
@@ -5,11 +5,17 @@
         <v-container>
           <div :style="{ paddingLeft: '55%' }">
             <h1 :style="{ fontSize: '40px', color: 'white' }">
-              Checkout
+              Ticket Details
             </h1>
           </div>
         </v-container>
       </v-col>
+      <v-col cols="4" align-self="center" class="pa-5">
+        <div class="timer">
+          {{min}}:{{sec}}
+        </div>
+      </v-col>
+      
       <v-container>
         <v-row>
           <v-col cols="6">
@@ -22,22 +28,24 @@
           <v-col cols="6" align-self="center" class="pa-5">
             <v-sheet color="black" fluid>
               <v-container>
-                <p class="text-h5 mb-5" style="columns: white">
-                  Blackpink concert (Kpop)
+                <p class="text-h5 mb-5" style="columns: white"> 
+                  Concert Name:
+                  {{ concertDetails.concert_name }}
                 </p>
                 <p class="text-h5 mb-5" style="columns: white">
-                  Number of tickets: {{ ticket_quantity }}
+                  Number of tickets: 
+                  {{ this.tix_quantity }}
                 </p>
                 <p class="text-h5 mb-5" style="columns: white">
-                  Total Price of Tickets: ${{ totalPrice }}
+                  Total Price: 
+                  ${{ this.totalPrice }}
                 </p>
               </v-container>
               <v-container>
                 <div>
                   <div id="paypal-button-container"></div>
                 </div>
- 
- 
+
                 <!-- peishan i disable this one first ah cos i dunno what this does haha, if u need it just uncomment -->
                 <!-- <div>
                 <PayPalScriptProvider :options="{ 'client-id': AYX78yVruw2aUiVYzwFdKMlWR9P771QpGLZqTdxbBBlkizAMYYzAP16GK4SPI63L4ih7Nhu9wm9BDpxu }">
@@ -51,37 +59,34 @@
       </v-container>
     </v-row>
   </v-container>
- </template>
- 
- 
- <style>
- #bg {
+</template>
+
+<style>
+#bg {
   background-image: linear-gradient(#01002c, #01010c, #040311);
   height: 100vh;
   background-repeat: no-repeat;
- }
- 
- 
- html {
-  background-color: black;
- }
- </style>
- 
- 
- <script>
- import { loadScript } from "@paypal/paypal-js";
- import axios from "axios";
- 
- 
- 
- 
- // i commented out the below cos im using the paypal-js package to make the buttons come out, if u need to enter the client if use the
- // code on top
- 
- 
- export default {
-  name: "PaymentPage",
+}
 
+html {
+  background-color: black;
+}
+
+.timer {
+      color: white;
+      font-size: 2rem;
+      font-weight: bolder;
+      text-align: center;
+      margin: 15px 0;
+    }
+</style>
+
+<script>
+import { loadScript } from "@paypal/paypal-js";
+import axios from "axios";
+
+export default {
+  name: "PaymentPage",
   async created() {
     this.concert_id = this.$route.params.concertid;
     this.userid = JSON.parse(localStorage.getItem('userid'));
@@ -91,25 +96,28 @@
     this.timeSec = JSON.parse(localStorage.getItem('timeSec'));
 
     await this.get_concert();
-
-    this.timeSec = JSON.parse(localStorage.getItem('timeSec'));
     this.seconds(); // start timer immediately, continue from seat selection pg
   },
-
   data() {
     return {
-      paymentStatus: null,
-      concert_id:null,
+      concertDetails: "",
+      hallDetails: "",
+      ticketAvailability: "",
+      ticketPrices: "",
       totalPrice: 0,
-      ticket_quantity: 0
- 
- 
- 
- 
+      paymentStatus: false,
+      timeSec: 5, // timer duration
     };
   },
+  computed: {
+      min() {
+        return String(Math.floor(this.timeSec/60)).padStart(2, '0');
+      },
+      sec() {
+        return String(this.timeSec%60).padStart(2, '0');
+      },
+  },
   methods: {
-
     seconds() {
       this.timeSec--;
       
@@ -124,8 +132,6 @@
               time.end();
           } else {
               time.timeSec--;
-              // store new time every sec
-              localStorage.setItem('timeSec', JSON.stringify(time.timeSec));
           }
       }, 1000);
     },
@@ -156,15 +162,11 @@
         // service offline, etc
         console.log(error);
       }
-    },    // Send notification if payment is successful
-
-    // Send notification if payment is successful, check paymentStatus value in data
-
+    },
+    // Send notification if payment is successful
     async sendNotif(paymentStatus) {
       if (paymentStatus) {
-        
-        const path = `http://127.0.0.1:5100/sendPaymentNotification/${this.userid}`;
-        
+        const path = `http://127.0.0.1:5100/testing`;
         axios
           .get(path)
           .then((res) => {
@@ -175,6 +177,29 @@
           });
       }
     },
+    async get_concert() {
+        //console.log("this.concert_id", this.concert_id);
+        try{
+          console.log("trying get_concert()");
+
+          const response = await axios.get(`http://127.0.0.1:5005/concert/${this.concert_id}`);
+          console.log("response", response);
+
+          if (response.data.length < 1) { //no data
+            console.log("totally not cryin");
+          }
+          else{
+            console.log("get_concert() works!");
+            this.concertDetails=response.data[0];
+
+          }
+        } catch (error) {
+          // Errors when calling the service; such as network error, 
+          // service offline, etc
+          console.log(error);
+        }
+
+      },
   },
   mounted() {
     loadScript({ "client-id": "test" })
@@ -182,76 +207,34 @@
         console.log(paypal.data);
         paypal
           .Buttons({
-            createOrder: function(data, actions) {
-        return actions.order.create({
-          purchase_units: [{
-            amount: {
-              value: '0.01'
-            }
-          }],
-          application_context: {
-            return_url: "https://localhost:8080/PaymentPage/"+this.concert_id, // sets the return URL to the current page
-           
-          }
-        });
-      },
-      onApprove(data,actions) {
-             console.log(data)
-             // This function captures the funds from the transaction.
-            
-             return actions.order.capture({
-         commit: true
-       })
-               // .then((response) => response.json())
-               .then((details) => {
-                 // This function shows a transaction success message to your buyer.
-                 this.paymentStatus = true
-                 // this.sendNotif(this.paymentStatus)
-                 alert(
-                   "Transaction completed by " + details.payer.name.given_name
-                 );
-
-
-               });
-           },
-         })
-
+            onApprove: (data) => {
+              console.log(data);
+              // Capture the funds from the transaction
+              return fetch("/my-server/capture-paypal-order", {
+                method: "POST",
+              })
+                .then((response) => response.json())
+                .then((details) => {
+                  // Show a transaction success message to the buyer
+                  this.paymentStatus = true;
+                  this.sendNotif(this.paymentStatus);
+                  alert(
+                    "Transaction completed by " +
+                      details.payer.name.given_name
+                  );
+                });
+            },
+          })
           .render("#paypal-button-container")
           .catch((error) => {
-            console.error("failed to render the PayPal Buttons", error);
+            console.error("Failed to render the PayPal buttons", error);
           });
       })
       .catch((error) => {
-        console.error("failed to load the PayPal JS SDK script", error);
+        console.error("Failed to load the PayPal JS SDK script", error);
       });
- 
- 
-   
-      this.totalPrice=this.$route.params.total_price
-      this.ticket_quantity=this.$route.params.ticket_quantity
- 
- 
-     
-      localStorage.setItem('paymentStatus', JSON.stringify(this.paymentStatus))
-      console.log(this.pa)
- 
- 
-      localStorage.setItem('concert_id', JSON.stringify(this.concert_id))
-      this.ticket_quantity=JSON.parse(localStorage.getItem('tix_quantity'))
-      this.totalPrice=JSON.parse(localStorage.getItem('totalPrice'))
-     
-     
+
+    localStorage.setItem("paymentStatus", JSON.stringify(this.paymentStatus));
   },
-   // mounted() {
-  //   // Add PayPal script to the document
-  //   const script = document.createElement('script')
-  //   script.src = 'https://www.paypal.com/sdk/js?client-id=AYX78yVruw2aUiVYzwFdKMlWR9P771QpGLZqTdxbBBlkizAMYYzAP16GK4SPI63L4ih7Nhu9wm9BDpxu&components=buttons'
-  //   script.addEventListener('load', () => {
-  //     // Render PayPal buttons
-  //     paypal.Buttons().render('#paypal-button-container')
-  //   })
-  //   document.body.appendChild(script)
-  // },
- };
- </script>
- 
+};
+</script>
